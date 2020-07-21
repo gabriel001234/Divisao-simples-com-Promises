bd7743ec1e617c3b9313e78f1b4c94a2ccb91114
Gabriel Paneque Didi <67348198+gabriel001234@users.noreply.github.com>
2020-07-20 22:15:54 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// função que retorna uma promise, dividindo x por y se y for diferente de 0
+
 function divisao(x, y) {
   return new Promise(function (resolve, reject) {
     if (y === 0) {
@@ -7,6 +9,7 @@ function divisao(x, y) {
   });
 }
 
+// teste 1
 divisao(12, 2)
   .then(function (res) {
     console.log('Resultado da divisão: ' + res);
@@ -14,6 +17,8 @@ divisao(12, 2)
   .catch(function (err) {
     console.log('Falha na divisão: ' + err);
   });
+
+// teste 2
 divisao(12, 0)
   .then(function (res) {
     console.log('Resultado da divisão: ' + res);
